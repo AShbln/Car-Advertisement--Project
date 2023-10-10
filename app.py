deec766aa8b5a39c9ea116bdfac5dc4b25179c28
AShbln <146025859+AShbln@users.noreply.github.com>
2023-10-11 00:31:04 +0300

    v.0.1.5

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-# import urllib.request
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 from PIL import Image
 # import altair
+# import urllib.request
 
 df = pd.read_csv('vehicles_us.csv')
+df.drop_duplicates()
+df = df.dropna(subset=['model_year', 'odometer'])
 df["brand"] = df['model'].str.split().str[0]
 brands = df.brand.unique()
 brands.sort()
@@ -24,12 +26,13 @@ year_range = st.sidebar.slider(
     "What is your year range?",
     min_value=1900,
     max_value=2023,
-    value=(1990, 2010))
+    value=(2000, 2020))
 year_range = list(range(year_range[0], year_range[1]+1))
 
 price_range = st.sidebar.slider(
     "What is your price range?",
     value=(0, 375000), step=500)
+
 
 selected_brands = st.sidebar.multiselect(
     'Select brand of car',
@@ -40,22 +43,36 @@ filtered_df = df[df['price'] <= price_range[1]]
 filtered_df = filtered_df[filtered_df['price'] >= price_range[0]]
 filtered_df = filtered_df[filtered_df.model_year.isin(year_range)]
 filtered_df = filtered_df[filtered_df.brand.isin(selected_brands)]
+num_rows = filtered_df.count()[0]
 
-fig2 = px.scatter(filtered_df, x="odometer", y="price", color='brand',
-                  title='Dependence of price on car mileage',
-                  range_y=(0, filtered_df.price.max()),
-                  trendline="ols", trendline_scope="overall",
-                  trendline_color_override="black")
-st.write('Diagram 1.', fig2)
+if num_rows > 0:
 
-fig1 = px.histogram(filtered_df, x="brand", color='fuel',
-                    title='Distribution of brands and fuel type')
-st.write('Diagram 2.', fig1)
+    fig2 = px.scatter(filtered_df, x="odometer", y="price", color='brand',
+                      title='Dependence of price on car mileage',
+                      range_y=(0, filtered_df.price.max()+5000),
+                      trendline="ols", trendline_scope="overall",
+                      trendline_color_override="black")
+    st.write('Diagram 1.', fig2)
 
-fig3 = px.histogram(filtered_df, x="model_year",
-                    title='Distribution of years of manufacture of the car')
-st.write('Diagram 3.', fig3)
+    st.write('Diagram 2.')
+    if st.checkbox('Sorted diagram'):
+        fig1 = px.histogram(filtered_df, x="brand", color='fuel',
+                            title='Distribution of brands and fuel type').update_xaxes(categoryorder='total descending')
+    else:
+        #    sorted_filtered_df = filtered_df.sort()
+        fig1 = px.histogram(filtered_df, x="brand", color='fuel',
+                            title='Distribution of brands and fuel type').update_layout(yaxis_title="quantity")
+    st.write(fig1)
 
-fig = px.pie(filtered_df,  names='brand', hole=.3,
-             title='Distribution of brands')
-st.write('Diagram 4.', fig)
+    fig3 = px.histogram(filtered_df, x="model_year",
+                        title='Distribution of years of manufacture of the car',
+                        labels={'model_year': 'model year'}).update_layout(yaxis_title="quantity")
+    st.write('Diagram 3.', fig3)
+
+    fig = px.pie(filtered_df,  names='brand', hole=.3,
+                 title='Distribution of brands')
+    st.write('Diagram 4.', fig)
+
+else:
+    st.subheader(":red[Data not found. ☹️]")
+    st.write('Please change parametrs.')
